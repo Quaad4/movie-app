961e95172e5bd9633c9eaf4c1f197636149ad4c7
Quaad4 <ajwood1998@gmail.com>
2026-09-01 16:36:46 +0200

    add loading and error message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,9 +44,15 @@ function Home() {
                 <button type="submit" className="search-button">Search</button>
             </form>
 
+           <div className="error">{error}</div> 
+
+            {loading ? 
+            <div className="loading">Loading...</div> 
+            :             
             <div className="movies-grid">
                 {movies.map(movie => <MovieCard key={movie.id} movie={movie} /> )}
             </div>
+            }
         </div>
     )
 }
