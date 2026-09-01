a37b5d443cd66322367fb297be4a05e07459fd71
Quaad4 <ajwood1998@gmail.com>
2026-09-01 15:19:07 +0200

    add styling to all pages and components

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,9 @@
+import '../css/Favorites.css'
+
 function Favorites() {
     return (
         <div className="favorites-empty">
-            <h1>No Favorite movies yet</h1>
+            <h2>No Favorite movies yet</h2>
             <p>Start adding movies to your favorites and they will appear here!</p>
         </div>
     )
